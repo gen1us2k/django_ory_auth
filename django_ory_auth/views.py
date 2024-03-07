e1c149da2bd43389a0e09fa35f1414a299cde44b
boosh <boosh@users.noreply.github.com>
2024-03-07 14:42:36 +0000

    View to clear cached auth data

--- django_ory_auth/views.py
+++ django_ory_auth/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+def logout(request):
+    """
+    View that clears cached auth data. Configure Ory to redirect here 
+    on log out.
+    """
+    user_id_cache_key = f"userid_{request.COOKIES.get('sessionid')}"
+    cache.delete(user_id_cache_key)
+  
+    logout_cache_key = f"logout_{request.COOKIES.get('sessionid')}"
+    cache.delete(logout_cache_key)
+
+    return redirect('logout_redirect_url_name')
