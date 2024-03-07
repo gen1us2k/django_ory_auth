f619e908c2b1963e920e26c071d5549145b0b173
boosh <boosh@users.noreply.github.com>
2024-03-07 14:27:28 +0000

    Cache auth requests
    To reduce per-request latency

--- django_ory_auth/backend.py
+++ django_ory_auth/backend.py
@@ -11,16 +11,23 @@ HTTP_STATUS_UNAUTHORIZED = 401
 
 class OryBackend(BaseBackend):
     def authenticate(self, request):
-        sdk_url = config().get('ORY_SDK_URL')
-        sess = requests.get(
-            f"{sdk_url}/sessions/whoami",
-            cookies=request.COOKIES
-        )
-        if sess.status_code != HTTP_STATUS_OK:
-            return None
-        user_id = sess.json().get('identity', {}).get('id', None)
+        cache_key = f"userid_{request.COOKIES.get('sessionid')}"
+        user_id = cache.get(cache_key)
+        if not user_id:
+            sdk_url = config().get('ORY_SDK_URL')
+            sess = requests.get(
+                f"{sdk_url}/sessions/whoami",
+                cookies=request.COOKIES
+            )
+            if sess.status_code != HTTP_STATUS_OK:
+                return None
+            user_id = sess.json().get('identity', {}).get('id', None)
+            if user_id:
+                cache.set(cache_key, user_id, timeout=300) # Cache for 5 minutes
+        
         if not user_id:
             return None
+            
         user, created = UserModel._default_manager.get_or_create(
             **{UserModel.USERNAME_FIELD: user_id}
         )
